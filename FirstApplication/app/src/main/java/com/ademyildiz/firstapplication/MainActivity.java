bd0caa2534aa545b70be085106270a443c2f5fc4
Adem Yıldız <107084880+10adem@users.noreply.github.com>
2023-10-28 08:43:05 +0300

    FirstApplication
    Here is the first information I learned for Android development with Java.

---
+++ FirstApplication/app/src/main/java/com/ademyildiz/firstapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ademyildiz.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeImage(View view) {
+        ImageView dumanImage = findViewById(R.id.duman);
+        dumanImage.setImageResource(R.drawable.images2);
+    }
+}
